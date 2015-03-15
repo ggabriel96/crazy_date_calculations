b211fd21940ba9bcc903035af1ce8b6dc3e7773f
Gabriel Galli <g7.galli96@gmail.com>
2015-03-15 20:47:25 -0300

    Moved the time converting methods to where they should be: the Time class.

--- Decoder.java
+++ Decoder.java
@@ -84,98 +84,14 @@ class Decoder {
 
     private double convert(int op, Time d) {
         switch (op) {
-            case 0: return this.seconds(d);
-            case 1: return this.minutes(d);
-            case 2: return this.hours(d);
-            case 3: return this.days(d);
-            case 4: return this.weeks(d);
-            case 5: return this.months(d);
-            case 6: return this.years(d);
+            case 0: return d.toSeconds();
+            case 1: return d.toMinutes();
+            case 2: return d.toHours();
+            case 3: return d.toDays();
+            case 4: return d.toWeeks();
+            case 5: return d.toMonths();
+            case 6: return d.toYears();
         }
         return 0;
-    }
-
-    private int seconds(Time d) {
-        int s = 0;
-        s += d.yr * 365 * 24 * 60 * 60;
-        s += d.mon * 30 * 24 * 60 * 60;
-        s += d.week * 7 * 24 * 60 * 60;
-        s += d.day * 24 * 60 * 60;
-        s += d.hour * 60 * 60;
-        s += d.min * 60;
-        s += d.sec;
-        return s;
-    }
-
-    private double minutes(Time d) {
-        double m = 0.0;
-        m += d.yr * 365 * 24 * 60;
-        m += d.mon * 30 * 24 * 60;
-        m += d.week * 7 * 24 * 60;
-        m += d.day * 24 * 60;
-        m += d.hour * 60;
-        m += d.min;
-        m += d.sec / 60.0;
-        return m;
-    }
-
-    private double hours(Time d) {
-        double h = 0.0;
-        h += d.yr * 365 * 24;
-        h += d.mon * 30 * 24;
-        h += d.week * 7 * 24;
-        h += d.day * 24;
-        h += d.hour;
-        h += d.min / 60.0;
-        h += d.sec / 60.0 / 60.0;
-        return h;
-    }
-
-    private double days(Time d) {
-        double b = 0.0;
-        b += d.yr * 365;
-        b += d.mon * 30;
-        b += d.week * 7;
-        b += d.day;
-        b += d.hour / 24.0;
-        b += d.min / 60.0 / 24.0;
-        b += d.sec / 60.0 / 60.0 / 24.0;
-        return b;
-    }
-
-    private double weeks(Time d) {
-        double w = 0.0;
-        w += d.yr * 12 * 4;
-        w += d.mon * 4;
-        w += d.week;
-        w += d.day / 7.0;
-        w += d.hour / 24.0 / 7.0;
-        w += d.min / 60.0 / 24.0 / 7.0;
-        w += d.sec / 60.0 / 60.0 / 24.0 / 7.0;
-        return w;
-    }
-
-    private double months(Time d) {
-        double m = 0.0;
-        m += d.yr * 12;
-        m += d.mon;
-        m += d.week / 4.0;
-        m += d.day / 30.0;
-        m += d.hour / 24.0 / 30.0;
-        m += d.min / 60.0 / 24.0 / 30.0;
-        m += d.sec / 60.0 / 60.0 / 24.0 / 30.0;
-        return m;
-    }
-
-    private double years(Time d) {
-        double y = 0.0;
-        y += d.yr;
-        y += d.mon / 12.0;
-        y += d.week / 4.0 / 12.0;
-        y += d.day / 30.0 / 12.0;
-        y += d.hour / 24.0 / 30.0 / 12.0;
-        y += d.min / 60.0 / 24.0 / 30.0 / 12.0;
-        y += d.sec / 60.0 / 60.0 / 24.0 / 30.0 / 12.0;
-        return y;
     }
 }
--- Time.java
+++ Time.java
@@ -6,4 +6,88 @@ class Time {
         return ("(" + this.week + "w) " + this.day + "/" + this.mon + "/" + this.yr +
         " - " + this.hour + ":" + this.min + ":" + this.sec);
     }
+
+    public int toSeconds() {
+        int s = 0;
+        s += this.yr * 365 * 24 * 60 * 60;
+        s += this.mon * 30 * 24 * 60 * 60;
+        s += this.week * 7 * 24 * 60 * 60;
+        s += this.day * 24 * 60 * 60;
+        s += this.hour * 60 * 60;
+        s += this.min * 60;
+        s += this.sec;
+        return s;
+    }
+
+    public double toMinutes() {
+        double m = 0.0;
+        m += this.yr * 365 * 24 * 60;
+        m += this.mon * 30 * 24 * 60;
+        m += this.week * 7 * 24 * 60;
+        m += this.day * 24 * 60;
+        m += this.hour * 60;
+        m += this.min;
+        m += this.sec / 60.0;
+        return m;
+    }
+
+    public double toHours() {
+        double h = 0.0;
+        h += this.yr * 365 * 24;
+        h += this.mon * 30 * 24;
+        h += this.week * 7 * 24;
+        h += this.day * 24;
+        h += this.hour;
+        h += this.min / 60.0;
+        h += this.sec / 60.0 / 60.0;
+        return h;
+    }
+
+    public double toDays() {
+        double d = 0.0;
+        d += this.yr * 365;
+        d += this.mon * 30;
+        d += this.week * 7;
+        d += this.day;
+        d += this.hour / 24.0;
+        d += this.min / 60.0 / 24.0;
+        d += this.sec / 60.0 / 60.0 / 24.0;
+        return d;
+    }
+
+    public double toWeeks() {
+        double w = 0.0;
+        w += this.yr * 12 * 4;
+        w += this.mon * 4;
+        w += this.week;
+        w += this.day / 7.0;
+        w += this.hour / 24.0 / 7.0;
+        w += this.min / 60.0 / 24.0 / 7.0;
+        w += this.sec / 60.0 / 60.0 / 24.0 / 7.0;
+        return w;
+    }
+
+    public double toMonths() {
+        double m = 0.0;
+        m += this.yr * 12;
+        m += this.mon;
+        m += this.week / 4.0;
+        m += this.day / 30.0;
+        m += this.hour / 24.0 / 30.0;
+        m += this.min / 60.0 / 24.0 / 30.0;
+        m += this.sec / 60.0 / 60.0 / 24.0 / 30.0;
+        return m;
+    }
+
+    public double toYears() {
+        double y = 0.0;
+        y += this.yr;
+        y += this.mon / 12.0;
+        y += this.week / 4.0 / 12.0;
+        y += this.day / 30.0 / 12.0;
+        y += this.hour / 24.0 / 30.0 / 12.0;
+        y += this.min / 60.0 / 24.0 / 30.0 / 12.0;
+        y += this.sec / 60.0 / 60.0 / 24.0 / 30.0 / 12.0;
+        return y;
+    }
 }
